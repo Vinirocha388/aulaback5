4492c9c121b65c1023595cce200a422dfd568a28
Vinícius Rocha <vinicius.a.rocha8@aluno.br>
2024-08-29 10:50:01 -0300

    feat:add some requests

--- src/server.js
+++ src/server.js
@@ -8,9 +8,63 @@ const app = express()
 
 app.use(express.json())
 
+const emocoes = [
+    {
+        id: 1,
+        nome: "Nojinho",
+        cor: "Verde"
+    },
+
+    {
+        id: 2,
+        nome: "Ansiedade",
+        cor: "Laranja"
+    },
+
+    {
+        id: 3,
+        nome:"Raiva",
+        cor: "Vermelho"
+    },
+]
+
 app.get("/", (req, res) => {
     return res.status(200).send({ message:"Hello World!"})
 })
+
+
+app.get("/emocoes", (req, res) => {
+    return res.status(200).send( emocoes )
+})
+
+const personagens = [   
+    {   
+        id:100,
+        nome: "Totoro",
+        vivo: true
+    },
+
+    {   
+        id:101,
+        nome: "Shrek",
+        filme: "Shrek 1,2,3,4",
+        vivo: true
+        
+    },
+
+    {   
+        id:102,
+        nome: "Jakob",
+         filme: "Velozes e furiosos X",
+        vivo: false
+
+    }
+]
+
+app.get("/personagens", (req, res) => {
+    return res.status(200).send( personagens )
+})
+
 
 app.get("/2tds2", (req, res) => {
     return res.status(200).send({ message:"Hello World!"})

104368e4b50454a2f19f89760f952047b6fe895d
Vinícius Rocha <vinicius.a.rocha8@aluno.br>
2024-09-26 15:13:58 -0300

    feat: create delete emotion method

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -77,4 +77,22 @@ emocoesRoutes.put("/:id", (req, res) => {
       });
 });
 
+emocoesRoutes.delete("/:id", (req, res) => {
+    const {id} = req.params;
+
+    const emocao = emocoes.find ((emotion) => emotion.id == id )
+
+    if (!emocao) {
+        return res.status(404).send({
+            message: "Emoção não encontrada!",
+        });
+    }
+
+    emocoes = emocoes.filter((emotion) => emotion.id = id)
+
+    return res.status(200).send({
+        message: "Emoção deletada",
+    });
+});
+
 export default emocoesRoutes;
